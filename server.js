a60fc3dab3308a7c8fd1eb3b37b135e4c1662ce4
Chaitanya Sharma <62926067+chaitanyas22@users.noreply.github.com>
2024-12-02 09:58:56 +0000

    Further changes

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const connectDB = require('./config/db');
-
+const lessonRoutes = require('./routes/lessonRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 const app = express();
 const PORT = 5000;
 
-// Connect to MongoDB
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/lessons', lessonRoutes);
+app.use('/api/orders', orderRoutes);
+
+// Start server
 connectDB().then(() => {
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
   });
+});
